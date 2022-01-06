b53945e6452216c4cd868b4baa7b280ee3caa336
MountainCat1 <72751263+MountainCat1@users.noreply.github.com>
2022-01-07 00:58:50 +0100

    Changed register and login UI

--- ProjectStoreBlazor/ProjectStoreBlazor/Shared/Models/RegisterUserDto.cs
+++ ProjectStoreBlazor/ProjectStoreBlazor/Shared/Models/RegisterUserDto.cs
@@ -8,26 +8,34 @@ namespace ProjectStoreBlazor.Shared.Models
         [MinLength(3, ErrorMessage = "Username length must be between 3-32")]
         [MaxLength(32, ErrorMessage = "Username length must be between 3-32")]
         public string Username { get; set; }
-        [MinLength(1)]
-        [MaxLength(32)]
-        public string Name { get; set; }
+
         [MinLength(1, ErrorMessage = "Name length must be between 1-32")]
         [MaxLength(32, ErrorMessage = "Name length must be between 1-32")]
-        public string Surname { get; set; }
+        public string Name { get; set; }
+        
 
         [MinLength(1, ErrorMessage = "Surname length must be between 1-32")]
         [MaxLength(32, ErrorMessage = "Surname length must be between 1-32")]
+        public string Surname { get; set; }
 
-        public string Password { get; set; }
+        
+
         [MinLength(6, ErrorMessage = "Password length must be between 6-32")]
+        public string Password { get; set; }
+        
+
         [MaxLength(32)]
         [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
         public string ConfirmPassword { get; set; }
 
+
         [EmailAddress]
         public string Email { get; set; }
+
         public int RoleId { get; set; }=1;
+
         public DateTime Birthday { get; set; }
+
         [Phone]
         public string PhoneNumber { get; set; }
     }
